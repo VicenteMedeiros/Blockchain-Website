6a82387dc2c093f62b5482006bf905e9c43fd188
VicenteMedeiros <vicenteaml@hotmail.com>
2023-05-22 09:18:07 -0300

    DropDown-Menu em progresso

--- script.js
+++ script.js
@@ -68,3 +68,18 @@ function initModal() {
   initTooltip();
   
   
+  function initDropdownMenu() {
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+    dropdownMenus.forEach(menu => {
+      ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+      });
+    });
+  
+    function handleClick(event) {
+      event.preventDefault();
+      this.classList.add('active');
+    };
+  }
+  
+  initDropdownMenu();
